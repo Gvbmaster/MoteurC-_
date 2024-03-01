a4d1113409e52007a150f3bbd04dfc2cbf419634
gnangndong <gnangndong@gaming.tech>
2024-03-01 15:57:31 +0100

    En doit tout reprendre de 0 ( l'initialisation)

--- Moteur/framework.h
+++ Moteur/framework.h
@@ -3,13 +3,11 @@
 //
 
 #pragma once
-
-#include "targetver.h"
 #define WIN32_LEAN_AND_MEAN             // Exclure les en-têtes Windows rarement utilisés
 // Fichiers d'en-tête Windows
 #include <windows.h>
-// Fichiers d'en-tête C RunTime
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
-#include <tchar.h>
+#include "d3dx12.h"
+#include "DDSTextureLoader.h"
+#include "MathHelper.h"
+#include <stdio.h>
+#include <string>
