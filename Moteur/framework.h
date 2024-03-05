e0c575df20d085925a780fa425917a406621e507
BaldurLeDur <102220784+BaldurLeDur@users.noreply.github.com>
2024-03-05 10:15:13 +0100

    nhbiosfghg
    dsxfghwduihwdfhuipdwsf

--- Moteur/framework.h
+++ Moteur/framework.h
@@ -1,15 +1,34 @@
-// header.h : fichier Include pour les fichiers Include système standard,
-// ou les fichiers Include spécifiques aux projets
-//
-
 #pragma once
 
-#include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // Exclure les en-têtes Windows rarement utilisés
-// Fichiers d'en-tête Windows
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+//#define WIN32_LEAN_AND_MEAN             // Exclure les en-têtes Windows rarement utilisés
 #include <windows.h>
-// Fichiers d'en-tête C RunTime
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
-#include <tchar.h>
+#include <WindowsX.h>
+#include <wrl.h>
+#include <dxgi1_4.h>
+#include <d3d12.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <cstdint>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+#include <comdef.h>
+#include <fstream>
+#include <DirectXMathVector.inl>
+#include <DirectXMathMatrix.inl>
+#include "MathHelper.h"
+
+#include "d3dx12.h"
